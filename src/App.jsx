8da19a4f99ad1fe8a4b3332cafd81f1d5bd0de8b
SURYA PRAKASH <isurya2005@gmail.com>
2025-07-29 12:37:26 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD Cloud Devops</p>
       <p>This is a simple homepage deployed on <b>GitHub Actions</b> using Vite.</p>
+      <p>Done with Git Pages & Git Actions</p>
     </div>
   );
 }
